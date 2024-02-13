c5dbf865a4b19aaeb9475134587032c62acf2ec8
Gustavo Franco <gustavofrancojp@gmail.com>
2024-02-13 12:09:57 -0400

    Changed companies to use MySQL

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,39 +1,21 @@
-import { HttpService } from '@nestjs/axios';
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { firstValueFrom } from 'rxjs';
+import { Injectable } from '@nestjs/common';
+import { DbService } from '../db/db.service';
 
 @Injectable()
 export class CompaniesService {
-  constructor(
-    private readonly httpService: HttpService,
-    private env: ConfigService,
-  ) {}
-
-  url = this.env.get<string>('API_DB_URL');
+  constructor(private db: DbService) {}
 
   async findOne(id: number) {
     const company = [1, 2];
-    const data = {
-      query: `SELECT STRING(codigo) as codi_rev, nome as raza_rev, nome as fant_rev, inscEstadual as inse_rev, CNPJCPF as cnpj_rev, (CASE WHEN codigo=1 THEN '1100122' WHEN codigo=2 THEN '1100304' END) as muni_rev, telefone as fone_rev, 'sac@rical.com.br' as mail_rev FROM Cad.Empresa WHERE codigo=${id}`,
-    };
 
     if (!company.includes(id)) {
-      throw new NotFoundException();
+      id = 0;
     }
 
-    const response = await firstValueFrom(
-      this.httpService.post(this.url, data),
-    );
+    const select = `SELECT id as codi_rev, razaoSocial as raza_rev, 'RICAL' as fant_rev, inscEstadual as inse_rev, cnpjCpf as cnpj_rev, codigoIbge as muni_rev, telefone as fone_rev, email as mail_rev FROM empresa WHERE id=${id}`;
 
-    const empresas = response.data.result.content;
-    return empresas.map((empresa) => {
-      for (const prop in empresa) {
-        if (empresa[prop] == '') {
-          empresa[prop] = null;
-        }
-      }
-      return empresa;
-    });
+    const resp = await this.db.mysqlSelect(select);
+
+    return resp;
   }
 }
