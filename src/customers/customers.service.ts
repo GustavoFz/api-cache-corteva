0493c2b7cb8852106f932a677e39ba6ff926c698
Gustavo Franco <gustavofrancojp@gmail.com>
2024-02-17 12:37:23 -0400

    Ajuste conuslta de clientes

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -14,7 +14,30 @@ export class CustomersService {
 
     //idTipoNota=61 é referente as notas emitidas pelo portal do produtor
 
-    const select = `SELECT codEmpresa AS codi_rev, codCliente AS codi_cli, cnpjCpf AS cnpj_cli, inscEstadual AS insc_cli, razaoSocial AS nome_cli, endereco AS enco_cli, numeroEndereco AS nume_cli, bairro AS bair_cli, null AS comp_cli, cep AS cepc_cli, codigoIbge AS muni_cli, telefone AS tele_cli, null AS tel2_cli, null AS celu_cli, email AS emai_cli, DATE_FORMAT(dataRegistro, "%Y-%m-%d") AS duma_cli, null AS dnac_cli FROM cliente WHERE codEmpresa=${id} AND codCliente in (SELECT codCliente FROM notaSaida WHERE codEmpresa=${id} AND idTipoNota=61 GROUP BY codCliente)`;
+    const select = `
+    SELECT 
+      codEmpresa AS codi_rev,
+      codigo AS codi_cli,
+      cnpjCpf AS cnpj_cli,
+      inscEstadual AS insc_cli,
+      razaoSocial AS nome_cli,
+      endereco AS enco_cli,
+      numeroEndereco AS nume_cli,
+      bairro AS bair_cli,
+      null AS comp_cli,
+      cep AS cepc_cli,
+      codigoIbge AS muni_cli,
+      telefone AS tele_cli,
+      null AS tel2_cli,
+      null AS celu_cli,
+      email AS emai_cli,
+      DATE_FORMAT(dataRegistro, "%Y-%m-%d") AS duma_cli,
+      null AS dnac_cli 
+    FROM 
+      cliente_empresa2
+    WHERE 
+      codEmpresa=${id}
+    `;
 
     return await this.db.mysqlSelect(select);
   }
