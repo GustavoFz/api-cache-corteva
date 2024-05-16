0663b793b2d5f6548fe308e37afe2000a6ab01a7
Gustavo Franco <gustavofrancojp@gmail.com>
2024-05-16 11:37:40 -0400

    Ajuste billing

--- src/billings/billings.service.ts
+++ src/billings/billings.service.ts
@@ -17,7 +17,7 @@ export class BillingsService {
       "55" AS modl_ven, 
       itemNota.cfop AS oper_ven, 
       itemNota.cfop AS cfop_ven, 
-      nat.nome AS desc_oper, 
+      nat.nome AS desc_opr, 
       item.ncm AS ncmp_pro, 
       IF(mov.operacao=0, "S", "E") AS sina_opr, 
       CONVERT(SUM(CASE WHEN nota.codEmitente IN (204,216,224,610,621,622,2883,6382,10086,11111,11192,32586,32835) AND nota.idDestinatario IN (1,2) THEN itemNota.vlrTotal WHEN nota.idEmitente IN (1,2) AND nota.codDestinatario IN ("1||10264","1||11","1||12665","1||22","1||3","1||60","1||64","1||66","1||7085","1||77","1||8367","2|| 10264","2||11","2||12665","2||22","2||3","2||60","2||64","2||66","2||7085","2||77","2||8367") THEN itemNota.vlrTotal ELSE 0 END), DECIMAL(18,4)) AS valf_ven, 
@@ -44,7 +44,7 @@ export class BillingsService {
     WHERE 
       mov.codEmpresa=${id} AND mov.dataLancamento BETWEEN "${dateStart}" AND "${dateEnd}" 
     GROUP BY 
-      codi_rev, mes_ven, ano_ven, oper_ven, cfop_ven, desc_oper, sina_opr, ncmp_pro ORDER BY mov.datalancamento DESC
+      codi_rev, mes_ven, ano_ven, oper_ven, cfop_ven, desc_opr, sina_opr, ncmp_pro ORDER BY mov.datalancamento DESC
     `;
 
     return await this.db.mysqlSelect(select);
